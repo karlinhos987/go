75b4f52fc0799d0db76ccb22bdd519393d5bfa03
karlinhos987 <karlinhos987@hotmail.com>
2025-03-30 21:17:05 -0300

    Alterar nome e package

--- capitulo04Exemplo01/main.go
+++ capitulo04Exemplo01/main.go
@@ -1,1 +1,22 @@
-package capitulo04Exemplo01
+package main
+
+import (
+	"fmt"
+)
+
+var x bool
+
+func main() {
+	fmt.Println(x) //zero value
+	x := true
+	fmt.Println(x) //valor atribuido
+
+	x = (10 < 100)
+	y := 10 == 100
+	z := 10 > 100
+
+	fmt.Println(x)
+	fmt.Println(y)
+	fmt.Println(z)
+
+}
